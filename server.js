4ef0bbe7e768d3bd773641dfa7544d4bfa099e36
kaif shaikh <156075872+KAIF254@users.noreply.github.com>
2026-04-01 10:46:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const multer = require("multer");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 
 const upload = multer({ dest: "uploads/" });
 
-// API route
-app.post("/upload", upload.single("image"), (req, res) => {
-  
-  // For now dummy response
-  res.json({
-    result: "Person Standing 🧍"
-  });
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend Working ✅");
+});
 
+// Upload route
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log("Image received");
+  res.json({ result: "Person Standing 🧍" });
 });
 
 app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+  console.log("🔥 Server running on http://localhost:5000");
 });
